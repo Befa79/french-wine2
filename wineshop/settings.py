966befffcb97ca9490da2848d1d7229f7acbb746
Befa79 <robindubois79@hotmail.com>
2021-03-06 11:30:11 +0000

    msg

--- wineshop/settings.py
+++ wineshop/settings.py
@@ -123,6 +123,7 @@ if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
+    print("USING POSTGRES")
 else:
     DATABASES = {
         'default': {
@@ -130,7 +131,7 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
+    print("USING SQLITE")
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
